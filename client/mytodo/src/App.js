860dcb6fe5a654c1e61c3a97c7e7678ac2a9aaee
Apoorwa <apoorwa.saxena@gmail.com>
2022-03-31 02:07:17 +0100

    Auth0 with .env vars implemented

--- client/mytodo/src/App.js
+++ client/mytodo/src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.css";
 import InputTodo from "./components/InputTodo";
 import ListTodo from "./components/ListTodo";
+import LoginButton from "./components/Login.js";
+import LogoutButton from "./components/Logout";
+import Profile from "./components/Profile";
 //import Collapse from "./components/collapse/collapse";
 
 function App() {
   return (
     <>
       <div className="container">
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
         <InputTodo />
         <ListTodo />
       </div>
--- client/mytodo/src/index.js
+++ client/mytodo/src/index.js
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { getConfig } from "./config";
+import history from "./utils/history";
+
+const onRedirectCallback = (appState) => {
+  history.push(
+    appState && appState.returnTo ? appState.returnTo : window.location.pathname
+  );
+};
+
+const config = getConfig();
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  ...(config.audience ? { audience: config.audience } : null),
+  redirectUri: window.location.origin,
+  onRedirectCallback,
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider {...providerConfig}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
