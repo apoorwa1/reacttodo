1b4731ae68fa3b0dfa1fce141d5f00d8078fa057
Apoorwa <apoorwa.saxena@gmail.com>
2022-03-31 01:11:46 +0100

    .env

--- server/config.js
+++ server/config.js
@@ -1,5 +1,4 @@
 // this environment variable gets handed to us by heroku if we use the postgres add-on
 export const connectionString = process.env.DATABASE_URL;
-// export const connectionString =
-//   "postgres://hjrdhatiziidgj:3608d8259c860ba42e08d0c5fe5ebd42f531a5901b013c30ec153228a99c79ee@ec2-3-225-213-67.compute-1.amazonaws.com:5432/d6ab1uuj2j96v2";
+
 console.log(connectionString);
